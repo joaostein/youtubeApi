f81a4da12ebe3154d5492572a46627d5c9c413f4
Joao Stein <joaosteing@gmail.com>
2013-12-18 15:21:26 -0800

    Add a comment with variable names for each parameter that embedSWF receives

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -8,4 +8,5 @@ var atts = {
   id: "myytplayer"
 };
 
+// swfobject.embedSWF(swfUrl, htmlElementId, width, height, swfVersion, xiSwfUrlStr, flashvarsObj, paramsObj, attrObj)
 swfobject.embedSWF("http://www.youtube.com/v/7QBgK0_RbkE?enablejsapi=1&playerapiid=ytplayer&version=3", "ytapiplayer", "425", "356", "8", null, null, params, atts);
