f80f3244e795d52ffd99e64d2bb50e95aff10fd5
Joao Stein <joaosteing@gmail.com>
2013-12-18 15:10:31 -0800

    Using swfobject to display a youtube video using the video ID

---
+++ assets/javascripts/app.js
@@ -0,0 +1,11 @@
+// video id = 7QBgK0_RbkE
+
+var params = {
+  allowScriptAccess: "always"
+};
+
+var atts = {
+  id: "myytplayer"
+};
+
+swfobject.embedSWF("http://www.youtube.com/v/7QBgK0_RbkE?enablejsapi=1&playerapiid=ytplayer&version=3", "ytapiplayer", "425", "356", "8", null, null, params, atts);
