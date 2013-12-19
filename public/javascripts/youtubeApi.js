c35d92e18c5704b359ad3fac5a4d3658df586212
Joao Stein <joaosteing@gmail.com>
2013-12-18 16:37:56 -0800

    Rename variables to meaningul and more readable names

--- public/javascripts/youtubeApi.js
+++ public/javascripts/youtubeApi.js
@@ -5,13 +5,13 @@ var params = {
 };
 
 var atts = {
-  id: "myytplayer"
+  id: "playerContainer"
 };
 
 // swfobject.embedSWF(swfUrl, htmlElementId, width, height, swfVersion, xiSwfUrlStr, flashvarsObj, paramsObj, attrObj)
-swfobject.embedSWF("http://www.youtube.com/v/7QBgK0_RbkE?enablejsapi=1&playerapiid=ytplayer&version=3", "ytapiplayer", "425", "356", "8", null, null, params, atts);
+swfobject.embedSWF("http://www.youtube.com/v/7QBgK0_RbkE?enablejsapi=1&playerapiid=myPlayer&version=3", "youtubeApiPlayer", "425", "356", "8", null, null, params, atts);
 
 // To test the functions, we need to have a local server running (https://developers.google.com/youtube/js_api_reference#GettingStarted)
 var onYouTubePlayerReady = function (playerId) {
-  ytplayer = document.getElementById("myytplayer");
+  myPlayer = document.getElementById("playerContainer");
 };
