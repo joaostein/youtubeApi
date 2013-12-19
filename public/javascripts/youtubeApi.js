b3392e30f28a0e9d182e9cec175b9d3c42460901
Joao Stein <joaosteing@gmail.com>
2013-12-18 16:03:37 -0800

    Add a comment explaining why need a server to run the youtubeApi functions

--- public/javascripts/youtubeApi.js
+++ public/javascripts/youtubeApi.js
@@ -11,7 +11,7 @@ var atts = {
 // swfobject.embedSWF(swfUrl, htmlElementId, width, height, swfVersion, xiSwfUrlStr, flashvarsObj, paramsObj, attrObj)
 swfobject.embedSWF("http://www.youtube.com/v/7QBgK0_RbkE?enablejsapi=1&playerapiid=ytplayer&version=3", "ytapiplayer", "425", "356", "8", null, null, params, atts);
 
+// To test the functions, we need to have a local server running (https://developers.google.com/youtube/js_api_reference#GettingStarted)
 function onYouTubePlayerReady(playerId) {
   ytplayer = document.getElementById("myytplayer");
-  console.log('ae');
 }
